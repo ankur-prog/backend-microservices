d5e9639951e393d0006915b08c2ffd50d6f26c1f
Ankur Kushwaha <kushwahaankur30@gmail.com>
2023-06-14 15:14:26 +0200

    modified invetory service

--- inventory-service/src/main/java/com/ankur/prog/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/ankur/prog/inventoryservice/repository/InventoryRepository.java
@@ -3,9 +3,10 @@ package com.ankur.prog.inventoryservice.repository;
 import com.ankur.prog.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
 
-    Optional<Inventory> findBySkuCode(String skuCode);
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
--- inventory-service/src/main/java/com/ankur/prog/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/ankur/prog/inventoryservice/service/InventoryService.java
@@ -1,9 +1,12 @@
 package com.ankur.prog.inventoryservice.service;
 
+import com.ankur.prog.inventoryservice.dto.InventoryResponse;
 import com.ankur.prog.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -11,9 +14,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
+
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode)
-    {
-    return   inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                        InventoryResponse.builder().skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
+                ).toList();
     }
 }
